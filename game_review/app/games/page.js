2ac48b1967223ac8204a4d177d0ce40489a25a40
Sam <samuel.bracci58@ed.cna.nl.ca>
2026-06-03 16:21:23 -0230

    Fixed a bug and added a form for games webpage

--- game_review/app/games/page.js
+++ game_review/app/games/page.js
@@ -7,6 +7,17 @@ export default function GamesPage() {
     <div className="games-page bg-gray-400 sm:text-3xl md:text-4xl lg:text-5xl">
         <h1>Games page (Need to add forms)</h1>
             <Image src="/skyrim-special-edition.jpg" alt="Skyrim photo" width={700} height={1}/>
+            <form>
+                <div>
+                    <label htmlFor="email"></label>
+                    <input 
+                    id="email" 
+                    type="email" 
+                    name="email" 
+                    required  
+                    />
+                </div>
+            </form>
 
     </div>
     )
