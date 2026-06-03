96a44588c2f9964d43543ace7639dcdafbd2ed32
Sam <samuel.bracci58@ed.cna.nl.ca>
2026-06-02 22:07:28 -0230

    Added files to the website

--- game_review/app/about/page.js
+++ game_review/app/about/page.js
@@ -1,5 +1,17 @@
+import "./about.css"
+
 export default function AboutPage() {
     return (
-    <h1>About us page</h1>
+    /*Responsive web design using Tailwind*/
+    <div className="about-page bg-gray-400 sm:text-3xl md:text-4xl lg:text-5xl">
+
+        <h1>Welcome to our website!</h1>
+        <h1>Our game review website lets users</h1>
+        <ul>
+            <li>- View a selection of popular video games</li>
+            <li>- Write your own review on a video game</li>
+            <li>- See what others have to say on their own reviews</li>
+        </ul>
+    </div>
     )
 }
--- game_review/app/games/page.js
+++ game_review/app/games/page.js
@@ -1,5 +1,13 @@
+import "./games.css"
+import Image from "next/image"
+
 export default function GamesPage() {
     return (
-    <h1>Games page</h1>
+     /*Responsive web design using Tailwind*/
+    <div className="games-page bg-gray-400 sm:text-3xl md:text-4xl lg:text-5xl">
+        <h1>Games page (Need to add forms)</h1>
+            <Image src="/skyrim-special-edition.jpg" alt="Skyrim photo" width={700} height={1}/>
+
+    </div>
     )
 }
